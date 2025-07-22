81a562fd952d52b9f9623fd42b230cfd7040cebb
abiney1321 <abiney1321@gmail.com>
2025-07-22 22:23:23 +0000

    setup nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
+import "@/app/global.css"; // Import global styles
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
