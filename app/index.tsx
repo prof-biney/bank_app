08724670475101be7da5b9c92b7773d6a68ea470
abiney1321 <abiney1321@gmail.com>
2025-07-23 08:52:40 +0000

    setup tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl text-blue-600">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
