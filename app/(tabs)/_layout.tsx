039cd5763e4094556bfa48a6086ca81216163f7e
abiney1321 <abiney1321@gmail.com>
2025-07-25 19:30:27 +0000

    implement tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,59 @@
-import { Redirect, Slot } from "expo-router";
-import React from "react";
+import { Tabs } from "expo-router";
+import { Activity, CreditCard, House, User } from "lucide-react-native";
 
 export default function TabLayout() {
-  const isAuthenticated = false; // Replace with actual authentication logic
-
-  if (!isAuthenticated) return <Redirect href="/sign-in" />;
-
-  return <Slot />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderTopWidth: 1,
+          borderTopColor: "#F3F4F6",
+          height: 80,
+          paddingBottom: 20,
+          paddingTop: 8,
+        },
+        tabBarActiveTintColor: "#0F766E",
+        tabBarInactiveTintColor: "#9CA3AF",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ size, color }) => <House size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="cards"
+        options={{
+          title: "Cards",
+          tabBarIcon: ({ size, color }) => (
+            <CreditCard size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="activity"
+        options={{
+          title: "Activity",
+          tabBarIcon: ({ size, color }) => (
+            <Activity size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
