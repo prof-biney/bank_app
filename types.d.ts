503c9d1c4cefbfcafd06f7fbb495741723a27a64
abiney1321 <abiney1321@gmail.com>
2025-08-01 10:07:07 +0000

    add user type

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,11 @@
+import { Models } from "react-native-appwrite";
+
+export interface User extends Models.Document {
+  name: string;
+  email: string;
+  avatar: string;
+}
+
 interface CustomInputProps {
   placeholder?: string;
   value?: string;
